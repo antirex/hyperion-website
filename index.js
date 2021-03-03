3839c6cb8af4334dc9547147e2209bc98a7da42e
Anshul Bamb <anshulbamb22@gmail.com>
2021-03-03 11:40:14 +0530

    format + linting

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 (function ($) {
-    "use strict";
-    AOS.init();
-  })(jQuery);
-  
-  $(function () {
-    $('[data-toggle="popover"]').popover();
-  });
-  
-  $(window).on("load", function () {
-    $(".loader-wrapper").fadeOut("fast");
-  });
+  "use strict";
+  AOS.init();
+})(jQuery);
+
+$(function () {
+  $('[data-toggle="popover"]').popover();
+});
+
+$(window).on("load", function () {
+  $(".loader-wrapper").fadeOut("fast");
+});
